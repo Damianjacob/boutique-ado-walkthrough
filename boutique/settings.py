245b35820a2e356ec24a2c2f7f1efde440daa5b8
Damian Jacob <damianm.jacob@yahoo.com>
2022-03-31 17:41:54 +0100

    add bag context and delivery logic

--- boutique/settings.py
+++ boutique/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -173,3 +174,6 @@ LOGGING = {
         'level': 'INFO',
     },
 }
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
