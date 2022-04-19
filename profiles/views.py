e33a8cd2efeab520c15092ba39cf107fef5ed3b2
Damian Jacob <damianm.jacob@yahoo.com>
2022-04-19 11:17:23 +0100

    add profiles app

---
+++ profiles/views.py
@@ -0,0 +1,10 @@
+from multiprocessing import context
+from django.shortcuts import render
+
+
+def profile(request):
+    """display the user's profile"""
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
