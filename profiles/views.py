e9e0e39123f0a4043d40656d94bd7fa9d32b7451
Damian Jacob <damianm.jacob@yahoo.com>
2022-04-19 14:52:43 +0100

    update allauth templats and tested profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,14 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def profile(request):
     """display the user's profile"""
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
